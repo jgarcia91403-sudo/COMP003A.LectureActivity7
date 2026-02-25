4e93d77607bd32f5a1abc3dd6841fe75ed87afad
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-02-25 11:51:26 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace COMP003A.LectureActivity7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 85, 90, 78, 92, 88 };
+            Console.WriteLine("Displaying array values using for loop:");
+
+        }
+    }
+}
